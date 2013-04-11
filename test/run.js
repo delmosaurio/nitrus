880b591524e526df3a2cb58301b5fd66070338f5
Delmo <delmosaurio@gmail.com>
2013-04-10 23:53:45 -0300

    some test here

---
+++ test/run.js
@@ -0,0 +1,13 @@
+process.env.NODE_ENV = 'test';
+
+var fs = require('fs')
+  , mkdirp = require('mkdirp')
+  , assert = require('assert');
+
+describe('Running a empty test', function() {
+
+	before(function(done){
+	   done();
+	});
+
+});
